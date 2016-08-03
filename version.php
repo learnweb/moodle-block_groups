1514de18069377f04dde310b98e401c6974141e8
Nina Herrmann <n_herr03@uni-muenster.de>
2016-08-03 13:01:24 +0200

    Version number

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016071500;     // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016080300;     // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015111000;     // Requires this Moodle version.
 $plugin->component = 'block_groups'; // Full name of the plugin (used for diagnostics).
-$plugin->release = 'v3.0-r4';
+$plugin->release = 'v3.1-r1';
 $plugin->maturity = MATURITY_RC;
