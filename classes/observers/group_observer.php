d05d2b7de84f8e487752806e377c0026e9b143df
lenaherf <lherfeld@uni-muenster.de>
2026-05-20 18:08:38 +0200

    event observing and handling whenever the settings are not in default for groups

---
+++ classes/observers/group_observer.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Event observers for the groups block.
+ *
+ * @package    block_groups
+ * @copyright  2026 Lena Herfeldt
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_groups\observers;
+
+/**
+ * Observer for group events
+ */
+class group_observer {
+    /**
+     * Handles the group_created event.
+     * Automatically adds new groups to block_groups_hide table if show_groups_default_setting is enabled.
+     *
+     * @param \core\event\group_created $event
+     */
+    public static function group_created(\core\event\group_created $event) {
+        global $DB;
+
+        // Checking if the setting is enabled.
+        $showalways = get_config('block_groups', 'show_groups_default_setting');
+
+        if (!$showalways) {
+            return;
+        }
+
+        $groupid = $event->objectid;
+
+        // Insert directly into the block_groups_hide table to make it visible from the start.
+        if (!$DB->record_exists('block_groups_hide', ['id' => $groupid])) {
+            $DB->insert_record_raw('block_groups_hide', ['id' => $groupid], true, false, true);
+        }
+    }
+}
